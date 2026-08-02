8e26908a7d138462ca31e99a49d8940f0a2c9239
ajay birla <ajay.birla@gemsessence.com>
2026-08-02 22:13:56 +0530

    Added chat stream from apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-// import { Assistant } from "./assistants/googleai";
-import { Assistant } from "./assistants/openai";
+import { Assistant } from "./assistants/googleai";
+// import { Assistant } from "./assistants/openai";
 import { Loader } from "./components/Loader/Loader";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
@@ -10,6 +10,17 @@ function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isStreaming, setIsStreaming] = useState(false);
+
+  function updateLastMessageContent(content) {
+    setMessages((prevMessages) =>
+      prevMessages.map((message, index) =>
+        index === prevMessages.length - 1
+          ? { ...message, content: `${message.content}${content}` }
+          : message
+      )
+    );
+  }
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -19,16 +30,28 @@ function App() {
     addMessage({ content, role: "user" });
     setIsLoading(true);
     try {
-      const result = await assistant.chat(content, messages);
-      addMessage({ content: result, role: "assistant" });
+      const result = await assistant.chatStream(content);
+      let isFirstChunk = false;
+
+      for await (const chunk of result) {
+        if (!isFirstChunk) {
+          isFirstChunk = true;
+          addMessage({ content: "", role: "assistant" });
+          setIsLoading(false);
+          setIsStreaming(true);
+        }
+
+        updateLastMessageContent(chunk);
+      }
+
+      setIsStreaming(false);
     } catch (error) {
-      console.log(error)
       addMessage({
         content: "Sorry, I couldn't process your request. Please try again!",
         role: "system",
       });
-    } finally {
       setIsLoading(false);
+      setIsStreaming(false);
     }
   }
 
@@ -42,7 +65,7 @@ function App() {
       <div className={styles.ChatContainer}>
         <Chat messages={messages} />
       </div>
-      <Controls isDisabled={ isLoading } onSend={handleContentSend} />
+      <Controls isDisabled={ isLoading || isStreaming } onSend={handleContentSend} />
     </div>
   );
 }
